60dba9ed3fb9749aaa472aae3f7b7eac4df0736f
XiNGRZ <chenxingyu92@gmail.com>
2012-12-30 22:37:00 +0800

    bugfix

--- test/lib.test.js
+++ test/lib.test.js
@@ -33,12 +33,11 @@ describe('contributors.js', function() {
         })
       })
 
-      it('should callback an empty array if not in a git repo', function(done) {
-        instance.getEmails('/tmp', function(err, emails) {
-          should.not.exist(err)
-          emails.should.be.ok
-          emails.should.be.an.instanceOf(Array)
-          emails.should.be.empty
+      it('should callback an error if not in a git repo', function(done) {
+        instance.getEmails('/tmp', function(err) {
+          should.exists(err)
+          should.exists(err.code)
+          err.code.should.equal('GITError')
           done()
         })
       })
@@ -66,7 +65,7 @@ describe('contributors.js', function() {
       })
 
       it('should callback an error with status code 404 if not exists', function(done) {
-        instance.getProfile('(a fake email not exists)', function(err, user) {
+        instance.getProfile('(a fake email not exists)', function(err) {
           should.exists(err)
           should.exists(err.code)
           should.exists(err.status)
@@ -86,6 +85,29 @@ describe('contributors.js', function() {
 
       it('should return the instance of Contributors', function() {
         instance.fetch().should.be.an.instanceOf(Contributors)
+      })
+
+      it('should callback an object if successed', function(done) {
+        instance.fetch('./', function(err, result) {
+          should.not.exists(err)
+          result.should.be.ok
+          result.should.be.an.instanceOf(Object)
+          should.exists(result.generatedAt)
+          should.exists(result.contributors)
+          result.generatedAt.should.be.a('number')
+          result.contributors.should.be.an.instanceOf(Array)
+          done()
+        })
+      })
+
+      it('should callback an error if not in a git repo', function(done) {
+        instance.on('error', function() {}) // so that it won't be throwed by EventEmitter
+        instance.fetch('/tmp', function(err) {
+          should.exists(err)
+          should.exists(err.code)
+          err.code.should.equal('GITError')
+          done()
+        })
       })
 
     })
